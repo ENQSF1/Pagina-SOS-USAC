2da05e2b7728a3d53173014fc2517a28f5b67ef9
kevin hernandez <kevinhernandz15@gmail.com>
2023-07-31 22:31:34 -0600

    firma

--- src/components/SH.js
+++ src/components/SH.js
@@ -5,6 +5,9 @@ import { TiSocialPinterest } from "react-icons/ti";
 const SH = () => {
   return (
     <>
+      <h6 className="text-center bg-gray-600 flex justify-center items-center text-white font-extrabold ">
+        Hecho por Kevs
+      </h6>
       <div className="flex flex-col md:flex-row justify-center ">
         <div className="w-full md:w-1/4 flex items-center justify-center">
           <div>
