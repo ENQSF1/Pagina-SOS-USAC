524670433b4ed889ada2a952d75124bb7a936e0d
kevin hernandez <kevinhernandz15@gmail.com>
2023-11-06 03:08:00 -0600

    moka api

--- src/pages/Candidatos/[id].js
+++ src/pages/Candidatos/[id].js
@@ -85,7 +85,7 @@ export default function ProductPage({ data }) {
 
 export async function getStaticPaths() {
   try {
-    const res = await fetch("https://pagina-sos-usac.vercel.app/api/avo");
+    const res = await fetch("https://apimocha.com/sosusac/candidatos");
     const data = await res.json();
     const paths = data.map(({ id }) => ({ params: { id } }));
 
